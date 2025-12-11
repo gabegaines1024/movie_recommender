7d1cdf5aa156a30807ea9005df543108d2f14d90
Gabe <gabegaines1024@gmail.com>
2025-12-11 18:27:01 -0500

    initialized recommender file and finished loading and merging function

---
+++ src/recommender.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import ast #used for safely evaluating string representations of Python literals (lists, dicts)
+
+DATA_PATH = '../data/'
+N_RECOMMENDATIONS = 10
+
+def load_and_merge_data(movies+file, credits_file):
+    """loads and merges the movies and credits datasets"""
+    try:
+        movies = pd.read_csv(DATA_PATH + movies_file)
+        credits = pd.read_csv(DATA_PATH + credits_file)
+
+    except FileNotFoundError as e:
+        print(f"Error loading data: {e}. Ensure data files are in the '{DATA_PATH}' directory.")
+        return None
+
+    #merge the two DataFrames on 'id' and 'movie_id'
+
+    #rename movie_id to id for merge
+    credits.columns = ['id', 'title', 'cast', 'crew']
+    df = movies.merge(credits, on='id)
+
+    #keep only relevant columns
+    df = df[['id', 'title', 'genres', 'keywords', 'cast', 'crew', 'overview']]
+    return df
